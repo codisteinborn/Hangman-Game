57ac2d6e64573d4ec903dfd367666fa986e5885a
Codi Steinborn <codisteinborn@Codis-MacBook-Pro.local>
2018-01-22 16:32:35 -0600

    monday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,92 @@
-// Choosing winning words
-var winWords = ["booty", "treasure", "parrot", "gold", "pirate"];
-// var spellWinWords{
-//     booty: "b", "o", "o", "t", "y",
+var $ = function (sel) {
+    var elem = document.querySelectorAll(sel);
 
+    var text = function (str) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].innerText = str;
+        }
+    }
+    var addClass = function (className) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].classList.add(className);
+        }
+    }
+    var removeClass = function (className) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].classList.remove(className);
+        }
+    }
+    var on = function (action, cb) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(action, cb);
+        }
+    }
+    var publicAPI = {
+        element: elem,
+        text: text,
+        addClass: addClass,
+        removeClass: removeClass,
+        on: on
+    }
+    return publicAPI;
+}
+// Choosing random winning word and splitting letters into array
+var winWordsArr = ["booty", "treasure", "parrot", "gold", "pirate", "ship"];
+var winWordRand = winWordsArr[Math.floor(Math.random() * winWordsArr.length)];
+console.log(winWordRand);
+var winWordLetArr = winWordRand.split("");
+console.log(winWordLetArr);
+
+// var ansArr = [];
+// var count = 0;
+// var blanks;
+// var startGame = function () {
+//     for (var i = 0; i < winWordRand.length; i++);
+//     {
+//         return ansArr[i] = " _ ";
+//         console.log(ansArr);
+//     }
+// blanks = ansArr.join(" ");
+// document.getElementById("guessBlanks").innerText = blanks;
+// console.log(blanks);
 // }
-// choosing random winning word
-var rand = winWords[Math.floor(Math.random() * winWords.length)];
-console.log(rand);
+// splitting words into characters and making that many blank spaces
+// var randChar = winWordRand.split("");
+// console.log(randChar);
+// var blanks = "_ ";
+// var blankSpaces = blanks.repeat(randChar.length);
+// console.log(blankSpaces);
+// // var guessBlankSpaces = document.getElementById("guessBlanks");
+// // console.log(guessBlankSpaces)
+// document.getElementById("guessBlanks").innerText = blankSpaces;
 
 
-// prompting users to guess letters
-var guess = prompt("Guess a letter");
-if(rand.indexOf(guess) > 0){
-alert("Correct");
+
+// document.addEventListener('keyup', event)
+// function logletter(event) {
+//     var guess = event.key;
+//     console.log(guess);
+// }
+
+var guess = prompt("pick a letter")
+console.log(guess);
+var lives = 7;
+var render = function () {
+    document.write(guess);
 }
-else{
-alert("Guess Again");
+for (var i = 0; i < winWordLetArr.length; i++) {
+    if (winWordRand.indexOf(guess) > 0) {
+        guess = prompt("Correct. Guess Again!");
+        console.log(guess);
+    }
+    else {
+        document.querySelector("#guessedLetters").innerText = render();
+        guess = prompt("Guess Again");
+        console.log(guess);
+        lives = lives - 1
+        console.log(lives);
+    }
 }
+
+
+
