b5321e0c6cfd446e60d4a5a01cbf83aa92b73750
Codi Steinborn <codisteinborn@Codis-MacBook-Pro.local>
2018-01-24 14:43:54 -0600

    new game resets guessed letters

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -10,45 +10,53 @@ var replace = document.querySelector("#guessBlanks").innerText = uArr.join(" ");
 
 var lives = 7;
 var wins = 0;
+var charArr = [];
+console.log(charArr);
 var allGuess = [];
 var isMatch = false;
 
 var begin = function (event) {
     var guess = event.key;
-    console.log(guess);
-    allGuess.push(guess);
+    console.log("guess", guess);
+    charArr.push(guess);
+    // console.log("charArr", charArr.push(guess))
     var isMatch = false;
     for (var i = 0; i < winWordLetArr.length; i++) {
         if (i < winWordLetArr.length) {
             if (winWordRand.indexOf(guess) >= 0 && lives > 0) {
-                // console.log(guess);
-                // console.log(winWordRand.indexOf(guess));
                 uArr[winWordRand.indexOf(guess)] = guess;
                 document.querySelector("#guessBlanks").innerText = uArr.join(" ");
                 isMatch = true;
+                console.log("winwordarrletlength", winWordLetArr.length)
+                console.log("all guess length", allGuess.length)
+                console.log("uarr length", uArr.length)
                 return guess;
             }
-            else if (lives === 0) {
-                alert("Walk the Plank");
-                lives = 7;
-                allGuess = [];
-                winWordRand = winWordsArr[Math.floor(Math.random() * winWordsArr.length)];
-                // console.log(winWordRand);
-                winWordLetArr = winWordRand.split("");
-                // console.log(winWordLetArr);
-                uArr = (winWordLetArr.fill(" _ ", 0));
-                // console.log(uArr);
-                replace = document.querySelector("#guessBlanks").innerText = uArr.join(" ");
-            }
+        }
+        else if (lives <= 0) {
+            alert("Walk the Plank");
+            lives = 7;
+            allGuess = [];
+            winWordRand = winWordsArr[Math.floor(Math.random() * winWordsArr.length)];
+            // console.log(winWordRand);
+            winWordLetArr = winWordRand.split("");
+            uArr = (winWordLetArr.fill(" _ ", 0));
+            // console.log(uArr);
+            replace = document.querySelector("#guessBlanks").innerText = uArr.join(" ");
         }
     }
     if (!isMatch) {
         lives--;
     }
-    // allGuess.push(guess);
+    allGuess.push(guess);
+    console.log("all guess", allGuess.length);
     $("#guessesRemain").text(lives);
     $("#win").text(wins);
     $("#guessedLetters").text(allGuess);
 
 }
 $("body").on("keyup", begin);
+
+
+
+
